7e2a96fe7fae31b1c6c5598edab9d8a7b2939e56
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-02-08 01:09:05 +0100

    leverage `getSubstrateConnector` from ScProvider

--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,3 +1,3 @@
 export * from "./connector/index.js"
 export { SupportedChains } from "./specs/index.js"
-export { ScProvider } from "./ScProvider.js"
+export { ScProvider } from "./ScProvider/index.js"
